3b5facf3eacdd28b6826a6d32a5e4f9cb091d740
Daryl Busson <daryljjbb@yahoo.com>
2026-08-04 19:19:19 -0700

    added Layout in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Tasks from "./pages/Tasks.jsx";
 import RequireRole from "./components/RequireRole.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import RequireAuth from "./components/RequireAuth.jsx";
+import Layout from "./components/Layout.jsx";
 
 
 
